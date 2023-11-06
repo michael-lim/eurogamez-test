db1f70aa92df0c8554995dfe6eed7f3b8613f391
michael-lim <93946005+michael-lim@users.noreply.github.com>
2023-11-06 11:03:29 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyCh3Ye9dSntSxvXYkvkMD9PKxazoiyByXU",
+  authDomain: "eurogamez-test.firebaseapp.com",
+  projectId: "eurogamez-test",
+  storageBucket: "eurogamez-test.appspot.com",
+  messagingSenderId: "536756149750",
+  appId: "1:536756149750:web:7757edfc2bd100e9263bd7",
+  measurementId: "G-S8QNB8J9M6"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>EuroGameZ Test !!!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <SafeAreaView>
+    <Text> Page Content </Text>
+  </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     // justifyContent: 'center',
+//   },
+// });
